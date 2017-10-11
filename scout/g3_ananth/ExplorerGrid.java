bf8d9ee2085ad9b50fbcc726eac19804619eb2b5
ananth2394 <6491429+ananth2394@users.noreply.github.com>
2017-10-10 22:54:04 -0400

    Add files via upload

---
+++ scout/g3_ananth/ExplorerGrid.java
@@ -0,0 +1,303 @@
+package scout.g3_ananth;
+
+import scout.sim.*;
+
+import java.util.*;
+import java.lang.Math;
+
+public class ExplorerGrid{
+    public int[][] grid;
+    public boolean[][] explored;
+    public static final String RED = "\033[0;31m";     // RED
+    public static final String GREEN = "\u001B[32m";   //GREEN
+    public static final String CYAN = "\u001B[36m";    //CYAN
+    public static final String YELLOW = "\u001B[33m";  //YELLOW
+    public static final String DEFAULT = "\u001B[0m";  //DEFAULT
+
+    public ExplorerGrid(int n) {
+        /*  the grid will contain what is located
+            0-unexplored
+            1-enemy
+            2-safe
+        */
+        this.grid = new int[n+2][n+2];
+        this.explored = new boolean [n+2][n+2];
+        for( int i = 0; i < this.grid.length ; i++){
+            for( int j = 0; j < this.grid[0].length; j++){
+                this.grid[i][j] = 8;
+                this.explored[i][j] = false;
+            }
+        }
+    }
+
+    public ExplorerGrid(ExplorerGrid another) {
+        int n = another.grid.length;
+        this.grid = new int[n][n];
+        this.explored = new boolean[n][n];
+        for( int i = 0; i < this.grid.length ; i++){
+            for( int j = 0; j < this.grid[0].length; j++){
+
+                  this.grid[i][j] = another.grid[i][j];
+                  this.explored[i][j] = another.explored[i][j];
+            }
+        }
+    }
+
+    // add enemy and safe location
+    public void addEnemy(Point location){
+        this.grid[location.x][location.y] = 1;
+    }
+
+    public void addSafe(Point location){
+        this.grid[location.x][location.y] = 2;
+    }
+
+    public void setLocation(Point location, int val)
+    {
+      this.grid[location.x][location.y] = val;
+    }
+
+    public int getLocation(Point location)
+    {
+      return this.grid[location.x][location.y];
+    }
+
+    public boolean getExplored(Point location)
+    {
+      return this.explored[location.x][location.y];
+    }
+
+    public void setExplored(Point location, boolean val)
+    {
+      if(val)
+      {
+        this.explored[location.x][location.y]=true;
+
+        // for(int i = -1;i<=1;i++)
+        // {
+        //   for(int j = -1;j<=1;j++)
+        //   {
+        //     int curx = location.x +i;
+        //     int cury = location.y+j;
+        //
+        //     if(0<= curx && curx < this.grid.length && 0<= cury && cury <this.grid.length )
+        //     {
+        //       if(!(curx == location.x && cury == location.y))
+        //       {
+        //         this.grid[i][j] = Math.max(this.grid[i][j]-1,0);
+        //       }
+        //     }
+        //   }
+        // }
+      }
+    }
+    // returns list of enemies or safe locations
+    private List<Point> getList(int type){
+      List<Point> listLocation = new ArrayList<>();
+      for( int i = 1; i < grid.length-1; i++){
+        for( int j = 1; j < grid[0].length-1; j++){
+            if(this.grid[i][j]==type){
+              listLocation.add(new Point(i,j));
+            }
+        }
+      }
+      return listLocation;
+    }
+    public List<Point> getEnemy(){
+        return getList(1);
+    }
+
+    public List<Point> getSafe(){
+      return getList(2);
+    }
+
+    public int[][] getGrid(){
+      return this.grid;
+    }
+
+    public boolean[][] getGridE(){
+      return this.explored;
+    }
+
+    public void setGrid(int [][] grid){
+      this.grid=grid;
+    }
+
+    public void setGridE(boolean [][] explored){
+      this.explored=explored;
+    }
+
+    public static void merge(ExplorerGrid A1, ExplorerGrid A2){
+      int[][] a1 = A1.getGrid();
+      int[][] a2 = A2.getGrid();
+
+      int a1val = -1;
+      int a2val = -1;
+
+      for( int i = 1; i < a1.length-1 ; i++){
+          for( int j = 1; j < a1[0].length-1; j++){
+              a1val = a1[i][j];
+              a2val = a2[i][j];
+
+              /*if( a1val != a2val ){
+                  if( a1val == 0 && a2val != 0 ){
+                      a1[i][j] = a2val;
+                  } else if( a1val != 0 && a2val == 0){
+                      a2[i][j] = a1val;
+                  } else{
+                      System.out.println("There is a mismatch!");
+                  }
+              }*/
+
+            int mn = Math.min(a1val,a2val);
+            a1[i][j] = mn;
+            a2[i][j] = mn;
+        }
+      }
+
+      A1.setGrid(a1);
+      A2.setGrid(a2);
+    }
+
+
+    public static void mergeE(ExplorerGrid A1, ExplorerGrid A2){
+      boolean[][] a1 = A1.getGridE();
+      boolean[][] a2 = A2.getGridE();
+
+      boolean a1val = false;
+      boolean a2val = false;
+
+      for( int i = 1; i < a1.length-1 ; i++){
+          for( int j = 1; j < a1[0].length-1; j++){
+              a1val = a1[i][j];
+              a2val = a2[i][j];
+
+              /*if( a1val != a2val ){
+                  if( a1val == 0 && a2val != 0 ){
+                      a1[i][j] = a2val;
+                  } else if( a1val != 0 && a2val == 0){
+                      a2[i][j] = a1val;
+                  } else{
+                      System.out.println("There is a mismatch!");
+                  }
+              }*/
+
+            boolean comb = a1val||a2val;
+            a1[i][j] = comb;
+            a2[i][j] = comb;
+        }
+      }
+
+      A1.setGridE(a1);
+      A2.setGridE(a2);
+    }
+    public void printGrid(){
+        int grid_size = grid.length;
+        //print col index
+        System.out.print(YELLOW +  "    |  " );
+        for(int i = 0; i < grid_size; i++){
+            System.out.print((i % grid_size)+ "  ");
+            if(i < 10){
+                System.out.print(" ");
+            }
+        }
+        System.out.println();
+        //print dashed line
+        for(int i = 0; i < grid_size*5; i++){
+            System.out.print("-");
+        }
+        System.out.println(DEFAULT);
+        for( int i = 0; i < grid_size; i++){
+          //print row number
+            System.out.print(YELLOW + (i % grid_size) + DEFAULT);
+            if(i < 10){
+                System.out.print(YELLOW + " "  + "  |  " + DEFAULT);
+            }
+            else{
+              System.out.print(YELLOW +  "  |  " + DEFAULT);
+            }
+            for( int j = 0; j < grid_size; j++){
+                if((i == 0 || i == grid_size -1) && (j == 0 || j == grid_size -1)){
+                  System.out.print(CYAN + "P" + DEFAULT + "  ");
+                  System.out.print(" ");
+                  continue;
+                }
+                else if(i == 0 || j == 0 || i == grid_size -1 || j == grid_size -1){
+                  System.out.print(CYAN + "0" + DEFAULT + "  ");
+                  System.out.print(" ");
+                  continue;
+                }
+                if(grid[i][j] == 1){
+                    System.out.print(RED + grid[i][j] + DEFAULT + "  ");
+                }
+                else if(grid[i][j] == 2){
+                    System.out.print(GREEN + grid[i][j] + DEFAULT + "  ");
+                }
+                else{
+                    System.out.print(grid[i][j] + "  ");
+                }
+
+                  System.out.print(" ");
+
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public void printGridE(){
+
+        boolean [][]grid = explored;
+        int grid_size = grid.length;
+        //print col index
+        System.out.print(YELLOW +  "    |  " );
+        for(int i = 0; i < grid_size; i++){
+            System.out.print((i % grid_size)+ "  ");
+            if(i < 10){
+                System.out.print(" ");
+            }
+        }
+        System.out.println();
+        //print dashed line
+        for(int i = 0; i < grid_size*5; i++){
+            System.out.print("-");
+        }
+        System.out.println(DEFAULT);
+        for( int i = 0; i < grid_size; i++){
+          //print row number
+            System.out.print(YELLOW + (i % grid_size) + DEFAULT);
+            if(i < 10){
+                System.out.print(YELLOW + " "  + "  |  " + DEFAULT);
+            }
+            else{
+              System.out.print(YELLOW +  "  |  " + DEFAULT);
+            }
+            for( int j = 0; j < grid_size; j++){
+                if((i == 0 || i == grid_size -1) && (j == 0 || j == grid_size -1)){
+                  System.out.print(CYAN + "P" + DEFAULT + "  ");
+                  System.out.print(" ");
+                  continue;
+                }
+                else if(i == 0 || j == 0 || i == grid_size -1 || j == grid_size -1){
+                  System.out.print(CYAN + "0" + DEFAULT + "  ");
+                  System.out.print(" ");
+                  continue;
+                }
+                if(grid[i][j] == true){
+                    System.out.print(RED + grid[i][j] + DEFAULT + "  ");
+                }
+                else if(grid[i][j] == false){
+                    System.out.print(GREEN + grid[i][j] + DEFAULT + "  ");
+                }
+                else{
+                    System.out.print(grid[i][j] + "  ");
+                }
+
+                  System.out.print(" ");
+
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
---
+++ scout/g3_ananth/Player.java
@@ -0,0 +1,209 @@
+package scout.g3_ananth;
+
+import scout.sim.*;
+import scout.g3_ananth.*;
+import java.util.*;
+
+
+
+//Read scout.sim.Player for more information!
+public class Player extends scout.sim.Player {
+    public PlayerHelper ph;
+    private int t,n,id;
+    private List<Point> landmarkLocations;
+    int stage = 0; /*
+
+    0: Find outpost
+    1: Explore board
+    2: Get back to outpost
+    */
+    boolean stage0;
+    int round=0;
+    LinkedList<String> cycle;
+    String[] checkmark = new String[2];
+
+    // for stage1 turning
+    int diagonalTurn = 0;
+
+    /**
+    * better to use init instead of constructor, don't modify ID or simulator will error
+    */
+    public Player(int id) {
+        super(id);
+        this.id=id;
+    }
+
+    /**
+    *   Called at the start
+    */
+    @Override
+    public void init(String id, int s, int n, int t, List<Point> landmarkLocations) {
+        this.t = t;
+        this.n = n;
+        this.landmarkLocations = landmarkLocations;
+        this.ph = new PlayerHelper(n);
+        this.stage0 = false;
+        this.stage = 0;
+        // change for each player
+        // this.cycle = new LinkedList<String>();
+        // switch(this.id%4){
+        //     case 0:
+        //         cycle.addAll(Arrays.asList(new String[]{"S","S","S","SE","SW","SW","W","NW","S","S"}));
+        //         checkmark=new String[]{"SE","NW"};
+        //         break;
+        //     case 1:
+        //         cycle.addAll(Arrays.asList(new String[]{"W","W","W","SW","NW","NW","N","NE","W","W"}));
+        //         checkmark=new String[]{"SW","NE"};
+        //         break;
+        //     case 2:
+        //         cycle.addAll(Arrays.asList(new String[]{"E","E","E","NE","SE","SE","S","SW","E","E"}));
+        //         checkmark=new String[]{"NE","SW"};
+        //         break;
+        //     case 3:
+        //         cycle.addAll(Arrays.asList(new String[]{"N","N","N","NW","NE","NE","E","SE","N","N"}));
+        //         checkmark=new String[]{"NW","SE"};
+        //         break;
+        // }
+
+    }
+
+    /**
+     * nearby IDs is a 3 x 3 grid of nearby IDs with you in the center (1,1) position. A position is null if it is off the board.
+     * Enemy IDs start with 'E', Player start with 'P', Outpost with 'O' and landmark with 'L'.
+     *
+     */
+    @Override
+    public Point move(ArrayList<ArrayList<ArrayList<String>>> nearbyIds, List<CellObject> concurrentObjects) {
+        this.ph.updatePlayerHelper(nearbyIds, concurrentObjects);
+        String moveDirection = "C";
+        List<String> moves = this.ph.getAllValidMoves(nearbyIds);
+
+        // Do your work here
+        // we move toward an outpost :)
+        if(this.stage == 0){
+            if(this.id%4==0){
+                moveDirection = toOutpost(moves,"NW", "N","W");
+            } else if(this.id%4==1){
+                moveDirection = toOutpost(moves,"NE", "N","E");
+            }else if(this.id%4==2){
+                moveDirection = toOutpost(moves,"SW", "S","W");
+            }else{
+                moveDirection = toOutpost(moves,"SE", "S","E");
+            }
+            if(moveDirection == "C"){
+                this.stage = 1;
+            }
+        }
+
+        else if(this.stage == 1)
+        {
+
+          moveDirection = this.ph.getRandomBestMove(nearbyIds);
+
+          
+        }
+        // if(this.stage0 == true){
+        //     String peekCycle = this.cycle.peek();
+        //     if(peekCycle == checkmark[0]){
+        //         if(!intersectionCheck()){
+        //             moveDirection = peekCycle;
+        //         } else {
+        //             moveDirection = popQueue();
+        //         }
+        //     } else if(peekCycle == checkmark[1]){
+        //         // hits boundary
+        //         if(!this.ph.hitBoundary()){
+        //             moveDirection = peekCycle;
+        //         } else {
+        //             moveDirection = popQueue();
+        //         }
+        //     } else {
+        //         moveDirection = popQueue();
+        //     }
+        //
+        //     if(this.ph.hitOutpost()){
+        //       round++;
+        //     }
+        //
+        // }
+        //
+        // if(round > 1){
+        //   if(this.ph.hitOutpost()){
+        //     if(this.ph.getAbsoluteX() == 0 && this.ph.getAbsoluteY() == 0){
+        //       moveDirection = "S";
+        //     } else if(this.ph.getAbsoluteX() == 0 && this.ph.getAbsoluteY() == n+1){
+        //       moveDirection = "W";
+        //     } else if(this.ph.getAbsoluteX() == n+1 && this.ph.getAbsoluteY() == 0){
+        //       moveDirection = "E";
+        //     } else {
+        //       moveDirection = "N";
+        //     }
+        //   }else{
+        //     if(!moves.contains("N")){
+        //       moveDirection = "W";
+        //     } else if(!moves.contains("E")){
+        //       moveDirection = "N";
+        //     } else if(!moves.contains("S")){
+        //       moveDirection = "E";
+        //     } else {
+        //       moveDirection = "S";
+        //     }
+        //   }
+        // }
+
+
+        // if(this.id%4==1 && this.ph.hitOutpost()){
+        //     this.ph.grid.printGrid();
+        // }
+
+        // make sure that you make your final move through PlayerHelper!
+
+        return this.ph.move(moveDirection);
+    }
+
+    public boolean intersectionCheck(){
+        switch (this.id%4){
+            case 0:
+                return (this.ph.getAbsoluteX() + this.ph.getAbsoluteY() == n);
+            case 1:
+                return (this.ph.getAbsoluteX() - this.ph.getAbsoluteY() == -1);
+            case 2:
+                return (this.ph.getAbsoluteX() - this.ph.getAbsoluteY() == 1);
+            case 3:
+                return (this.ph.getAbsoluteX() + this.ph.getAbsoluteY() == n+2);
+        }
+        return true;
+    }
+
+    public String popQueue(){
+        String temp = this.cycle.pop();
+        this.cycle.add(temp);
+        return this.cycle.peek();
+    }
+
+    public String toOutpost(List<String> moves, String initial, String e1, String e2){
+        if(moves.contains(initial)){
+            return initial;
+        } else if (moves.contains(e1)){
+            return e1;
+        } else if (moves.contains(e2)){
+            return e2;
+        }
+        return "C";
+    }
+
+    public void stub() {
+        ;
+    }
+
+    @Override
+    public void communicate(ArrayList<ArrayList<ArrayList<String>>> nearbyIds, List<CellObject> concurrentObjects) {
+        --t;
+    }
+
+    @Override
+    public void moveFinished() {
+        this.ph.updateCurrentLocation();
+    }
+
+}
